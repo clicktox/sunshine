bf43aa238b235cb5902e5eed900ffacb293b9f41
Justin Lane <justin@modernmom.com>
2013-07-25 19:05:21 +0000

    addd fields.py, updated models for scoop

--- modernmom/scoop/models.py
+++ modernmom/scoop/models.py
@@ -1,2 +1,16 @@
 from django.db import models
 from fields import UUIDField
+from photologue.models import ImageModel
+from fields import UUIDField
+
+class ScoopImage(ImageModel):
+    pass
+
+class ScoopItem(models.Model):
+    pass
+
+class ScoopTopic(models.Model):
+    name = models.CharField(max_length=255)
+    uuid = UUIDField()
+    pass
+
